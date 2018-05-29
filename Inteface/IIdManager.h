784837e32aded15348ecb5dfed7458f2dc71848c
Yan Minzhi <yanmingzhi@elastos.org>
2018-05-29 19:31:21 +0800

    #5 Add definition of id manager.

---
+++ Inteface/IIdManager.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2012-2018 The Elastos Open Source Project
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef __ELASTOS_SDK_IIDMANAGER_H__
+#define __ELASTOS_SDK_IIDMANAGER_H__
+
+#include "IIdManagerCallback.h"
+
+namespace Elastos {
+	namespace SDK {
+
+		class IIdManager {
+		public:
+
+			virtual nlohmann::json GenerateId(std::string &id, std::string &privateKey) = 0;
+
+			virtual nlohmann::json getLastIdValue(const std::string &path) = 0;
+
+			virtual nlohmann::json getIdHistoryValues(const std::string &id, const std::string &path) = 0;
+
+			virtual void AddCallback(IIdManagerCallback *managerCallback) = 0;
+
+			virtual void RemoveCallback(IIdManagerCallback *managerCallback) = 0;
+		};
+
+	}
+}
+
+#endif //__ELASTOS_SDK_IIDMANAGER_H__
